15a27bab5c0cf980c15989b38dcc087dde0961e2
Iyanu456 <oyerindei13@gmail.com>
2023-11-05 23:02:29 +0100

    ui improvements

--- src/app/components/Sidebar.js
+++ src/app/components/Sidebar.js
@@ -1,12 +1,24 @@
 import Image from "next/image"
+import Link from "next/link";
 import closeIcon from "../assets/icons/close.svg"
 
 export default function Sidebar(props) {
+
+	function closeSidebar() {
+		props.setSidebarOpen(false)
+	}
+
 	return (
 		<div className={`sidebar w-[100%] mobile ${props.sidebarOpen ? "open" : null}`}>
-		<div  className="mb-[2em] ml-auto mr-[1em]"><Image onClick={() => props.setSidebarOpen(false)} src={closeIcon} alt="close sidebar button" className="my-auto ml-auto mr-o" /></div>
-			<div className="sidebar-item">About me</div>
-			<div className="sidebar-item">Projects</div>
+		<div  className="mb-[2em] ml-auto mr-[1em]"><Image onClick={closeSidebar} src={closeIcon} alt="close sidebar button" className="my-auto ml-auto mr-o" /></div>
+		
+			<Link onClick={closeSidebar} href="#about-me" >
+				<div className="sidebar-item" >About me</div>
+			</Link>
+
+			<Link onClick={closeSidebar} href="#projects" >
+				<div className="sidebar-item" >Projects</div>
+			</Link>
 		</div>
 	)
 }
