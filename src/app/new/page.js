024848e51d46443af2fc3f9e996c2aa48d6ba3d8
iyanu456 <oyerindei13@gmail.com>
2024-10-21 00:28:01 +0100

    ui improvements

--- src/app/new/page.js
+++ src/app/new/page.js
@@ -248,7 +248,7 @@ export default function Page() {
       </section>
 
       <section
-        className="max-sm:grid my-auto pt-[2em] px-[2.5em] md:pl-[12em] md:pr-[5em]  min-h-[90vh] md:place-items-center max-sm:max-w-[100vw] lg:max-w-[93em] mx-auto"
+        className="max-sm:grid my-auto pt-[2em] px-[2.5em] md:pl-[12em] md:pr-[5em]  min-h-[90vh] md:place-items-center lg:max-w-[93em] mx-auto"
         id="projects"
       >
 
