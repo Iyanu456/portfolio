f1aed5dd4e51df8655b210cf09e621ebd06f9dec
iyanu456 <oyerindei13@gmail.com>
2024-10-21 01:51:19 +0100

    ui improvements

--- src/app/components/Navigation.js
+++ src/app/components/Navigation.js
@@ -124,7 +124,7 @@ export default function Navigation(props) {
     </header>
 
 	
-        <div className={`grid place-items-center md:hidden lg:hidden transition-transform transform duration-300 overflow-y-hidden fixed top-[4.9em] bottom-auto right-0 left-0 z-[1000] h-[100%] w-[100%] bg-[#112240] shadow-lg ${props.mobileMenuOpen ? "translate-x-0" : "translate-x-[100%] hidden"} `}>
+        <div className={`grid place-items-center md:hidden lg:hidden transition-transform transform duration-300 overflow-y-hidden fixed top-[4.9em] bottom-auto right-0 left-0 z-[1000] h-[100%] w-[80%] bg-[#112240] shadow-lg ${props.mobileMenuOpen ? " translate-x-0" : "translate-x-[180%]"} `}>
           <nav className="mx-auto flex flex-col gap-3 px-[2em] pt-2 pb-3 space-y-1 sm:px-[2em] w-[70%] text-center">
             <Link
               href="/"
